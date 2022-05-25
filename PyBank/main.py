20d7679557a6e8dc208fbbe8de30320b1921259a
Noumenal Aglet <king314159265@gmail.com>
2022-05-25 17:38:36 +1000

    refining

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,9 +2,9 @@
 import csv
 
 #Variables
-month_counter = 0
-#set up with a starting value so no change is detected in first month
-old_monthly_profit = 867884
+month_counter = 1
+#for modularity, this value is set below, at line 30
+old_monthly_profit = 0
 new_monthly_profit = 0
 net_total = 0
 profit_change = 0
@@ -15,6 +15,7 @@ month_lowest_change = ""
 sum_profit_changes = 0
 average_profit_change = 0
 output_lines = {}
+first_line = True
 
 #reading the csv file
 csv_path = (f"Resources/budget_data.csv")
@@ -23,27 +24,33 @@ with open(csv_path) as csv_file:
 	#skipping header
 	next(csv_reader)
 
+	#finds the 1st row and saves the profit value so there is no change calc. at line 38
 	for line in csv_reader:
-		#ignoring header, count every line for number of months
-		month_counter += 1
-		new_monthly_profit = int(line[1])
-		#finding the change of profits each month
-		profit_change = new_monthly_profit - old_monthly_profit
-		#recording highest or lowest change and corresponding month if record is broken
-		if profit_change >= current_highest_change:
-			current_highest_change = profit_change
-			month_highest_change = line[0]
-		elif profit_change < current_lowest_change:
-			current_lowest_change = profit_change
-			month_lowest_change = line[0]
-		#resetting monthly profit values for next iteration
-		old_monthly_profit = new_monthly_profit
-		#adding up all the changes in profit
-		sum_profit_changes += profit_change
-		#adding up the total amounts of profit
-		net_total += old_monthly_profit
-	#Working out the average change of profits, using the no. of months -1, as no change occuring during first month
-	average_profit_change = sum_profit_changes / (month_counter-1)
+		if first_line == True:
+			old_monthly_profit = int(line[1])
+			first_line = False
+
+			for line in csv_reader:
+				#count every line for number of months
+				month_counter += 1
+				new_monthly_profit = int(line[1])
+				#finding the change of profits each month
+				profit_change = new_monthly_profit - old_monthly_profit
+				#recording highest or lowest change and corresponding month if record is broken
+				if profit_change >= current_highest_change:
+					current_highest_change = profit_change
+					month_highest_change = line[0]
+				elif profit_change < current_lowest_change:
+					current_lowest_change = profit_change
+					month_lowest_change = line[0]
+				#resetting monthly profit values for next iteration
+				old_monthly_profit = new_monthly_profit
+				#adding up all the changes in profit
+				sum_profit_changes += profit_change
+				#adding up the total amounts of profit
+				net_total += old_monthly_profit
+			#Working out the average change of profits, using the no. of months -1, as no change occuring during first month
+			average_profit_change = sum_profit_changes / (month_counter-1)
 
 #creating output for printing and new analysis text file
 output_lines = {
