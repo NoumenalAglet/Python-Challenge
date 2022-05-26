f0f4717f315a8a7d9713242a2e0578b3d63d3943
Noumenal Aglet <king314159265@gmail.com>
2022-05-26 19:58:02 +1000

    PyBoss completed

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -1,18 +1,17 @@
 #PyBoss
 
 import csv
-#will use state abbreviations dict
 from us_state_abbrev import us_state_abbrev
 import os
 
 #variables
-complete_data = [] #could change name to local data or data
-id = []
-name = []
-dob = []
-ssn = []
-state = []
-
+line = []
+id = ""
+name = ""
+dob = ""
+ssn = ""
+state = ""
+complete_data = []
 
 #open Resources/employee_data.csv
 csv_path = os.path.join("Resources", "employee_data.csv")
@@ -21,22 +20,18 @@ with open(csv_path) as csv_file:
     #skipping header
     csv_header = next(csv_reader)
 
-  
     for line in csv_reader:
-        #adding a new variable which will need more cols later
-        complete_data = line
-        
-        #storing each column in a list, altering all but 'id'
-        id = complete_data[0]
+        #effectively storing each column in a list, altering as required
+        id = line[0]
         
         #splitting name then storing in different variables
-        name = complete_data[1]
+        name = line[1]
         name = name.split()
         first_name = name[0]
         last_name = name[1]
 
         #converting date from YYYY-MM-DD to MM/DD/YYYY
-        dob = complete_data[2]
+        dob = line[2]
         #sperating by '-'
         dob = dob.split('-')
         year = dob[0]
@@ -46,42 +41,33 @@ with open(csv_path) as csv_file:
         dob[0] = day
         dob[1] = month
         dob[2] = year       
-        dob = '/'.join([str(item) for item in dob])
+        dob = '/'.join(dob)
 
         #slicing out the digits to keep and str of *
-        ssn = complete_data[3]
+        ssn = line[3]
         slice_object = slice(6, 11, 1)
         ssn = ssn[slice_object]
         ssn = "***-**" + ssn
 
-        #find the state in the dictionary imported above and abbreviate
-        state = complete_data[4]
+        #finding the state in the dictionary imported above and abbreviating
+        state = line[4]
         if state in us_state_abbrev:
             state = us_state_abbrev[state]    
 
         #putting it all back together
-        complete_data[1] = first_name
-        complete_data[2] = last_name
-        complete_data[3] = dob
-        complete_data[4] = ssn
-        complete_data.append(state)
+        line[1] = first_name
+        line[2] = last_name
+        line[3] = dob
+        line[4] = ssn
+        line.append(state)
 
-        #list(complete_data)
-       # print(complete_data)
+        #appending value to complete_data, to be used when outputting file
+        complete_data.append(line)
 
-
-
-#output to Analysis/employee_data.csv
+#writing to new .csv file
 csv_path = os.path.join("Analysis", "Employee_Data_Formatted.csv")
 with open(csv_path, 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(['Emp ID', 'First Name', 'Last Name', 'DOB', 'SSN', 'State'])
     for x in complete_data:
-        csvwriter.writerows(complete_data)
-        print(complete_data) #for some reason complete data is 5 rows with last lists value
-        #could possible move indentation
-
-    #write the first row (column headers)
-  #  csvwriter.writerow(['Emp ID', 'First Name', 'Last Name', 'DOB', 'SSN', 'State'])
-    #write the second row
- #   csvwriter.writerow(['!214!', 'Sarah', 'Simpson', '12/04/1985', '***-**-8166', 'FL'])
-
+        csvwriter.writerow(x)
