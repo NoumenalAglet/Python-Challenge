ed75e6ce2053f8e71bd4a41cd91f07a85b47cd3a
Noumenal Aglet <king314159265@gmail.com>
2022-05-27 15:05:36 +1000

    folders change and starting on PyParagraph

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -13,15 +13,15 @@ ssn = ""
 state = ""
 complete_data = []
 
-#open Resources/employee_data.csv
+#open employee_data.csv from Resources folder
 csv_path = os.path.join("Resources", "employee_data.csv")
 with open(csv_path) as csv_file: 
     csv_reader = csv.reader(csv_file, delimiter=',') 
     #skipping header
     csv_header = next(csv_reader)
 
+    #cumulatively storing each line into a column, altering as required
     for line in csv_reader:
-        #effectively storing each column in a list, altering as required
         id = line[0]
         
         #splitting name then storing in different variables
@@ -43,7 +43,7 @@ with open(csv_path) as csv_file:
         dob[2] = year       
         dob = '/'.join(dob)
 
-        #slicing out the digits to keep and str of *
+        #slicing out the digits to keep and adding to str of *
         ssn = line[3]
         slice_object = slice(6, 11, 1)
         ssn = ssn[slice_object]
@@ -64,8 +64,8 @@ with open(csv_path) as csv_file:
         #appending value to complete_data, to be used when outputting file
         complete_data.append(line)
 
-#writing to new .csv file
-csv_path = os.path.join("Analysis", "Employee_Data_Formatted.csv")
+#writing to new employee_data.csv file in Formatted_Output folder
+csv_path = os.path.join("Formatted_Output", "employee_data.csv")
 with open(csv_path, 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=',')
     csvwriter.writerow(['Emp ID', 'First Name', 'Last Name', 'DOB', 'SSN', 'State'])
--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,10 +1,41 @@
 #PyParagraph
 
-import csv
+import os
 
 #defining variables
 
-csv_path = ('Resources/election_data.csv')
-with open(csv_path) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',') 
+#reading paragraph_1 in Resources
+file_path = os.path.join("Resources", "paragraph_1.txt")
+with open(file_path, 'r', encoding="UTF-8") as f:
 
+    #print(f.readline())
+    #print(f.readlines())
+
+    main_text = f.readline()
+
+    words = main_text.split()
+    #words = words.count()
+    #words are list doe
+    
+    
+    print(words)
+#approximate word count
+#split()
+#count()
+
+#approximate sentence count
+
+#average letter count
+
+#average sentence length
+
+
+#output to terminal
+'''
+Paragraph Analysis
+-----------------
+Approximate Word Count: 122
+Approximate Sentence Count: 5
+Average Letter Count: 4.6
+Average Sentence Length: 24.0
+'''
