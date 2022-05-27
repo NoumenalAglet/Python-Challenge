7ec69cf6a23580209ee5086d04d3287852210e14
Noumenal Aglet <king314159265@gmail.com>
2022-05-27 18:23:55 +1000

    getting into regex

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -5,16 +5,20 @@ import re
 
 #defining variables
 main_text = ''
+no_of_words = 0
+no_of_sentences = 0
+ave_letter_count = 0.0
+ave_sentence_len = 0.0
 
 #reading paragraph_1 in Resources
-file_path = os.path.join("Resources", "paragraph_1.txt")
+file_path = os.path.join("Resources", "paragraph_3.txt")
 with open(file_path, 'r', encoding="UTF-8") as f:
 
     #print(f.readline())
     #print(f.readlines())
 
-    
-    main_text = f.readline()
+
+    main_text = f.read()
     #print(main_text)
     
     #approximate word count
@@ -39,4 +43,13 @@ Approximate Word Count: 122
 Approximate Sentence Count: 5
 Average Letter Count: 4.6
 Average Sentence Length: 24.0
+'''
+
+'''
+reTest = '118-899-0703 859.030.9320 490*943*2039'
+pattern = re.compile(r'\d\d\d[-.]\d\d\d[-.]\d\d\d\d')
+matches = pattern.finditer(reTest)
+for match in matches:
+    print(match)
+print(reTest[178:179])
 '''
